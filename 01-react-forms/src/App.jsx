a33ab2ea1e5d85bd3421b8f37c7c333a12f3f4a8
César Guerra <warderer@gmail.com>
2024-06-24 21:17:39 -0600

    ✨ ReactHookForm base

--- 01-react-forms/src/App.jsx
+++ 01-react-forms/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ReactHookForm from './pages/ReactHookForm'
 import SimpleForm from './pages/SimpleForm'
 
 function App () {
   return (
     <>
       <SimpleForm />
+      <ReactHookForm />
     </>
   )
 }
