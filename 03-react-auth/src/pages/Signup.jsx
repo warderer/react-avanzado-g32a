6a6da30eae3ad15f374409d18ca9c410b2b99563
César Guerra <warderer@gmail.com>
2024-07-03 21:13:12 -0600

    ✨ basic react-hook-form on signup form

--- 03-react-auth/src/pages/Signup.jsx
+++ 03-react-auth/src/pages/Signup.jsx
@@ -1,10 +1,19 @@
+import { useForm } from 'react-hook-form'
 import '@/styles/form.css'
 import logo from '@/assets/react.svg'
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <main className='form-signin w-100 m-auto'>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img className='mb-4' src={logo} alt='' width='72' height='57' />
         <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
 
@@ -15,7 +24,9 @@ const Signup = () => {
             id='first_name'
             name='first_name'
             placeholder='John'
+            {...register('first_name', { required: true })}
           />
+          {errors.first_name && <span>This field is required</span>}
           <label htmlFor='first_name'>First Name</label>
         </div>
 
@@ -26,7 +37,9 @@ const Signup = () => {
             id='last_name'
             name='last_name'
             placeholder='Doe'
+            {...register('last_name', { required: true })}
           />
+          {errors.last_name && <span>This field is required</span>}
           <label htmlFor='last_name'>Last Name</label>
         </div>
 
@@ -35,11 +48,13 @@ const Signup = () => {
             className='form-select'
             id='gender'
             name='gender'
+            {...register('gender', { required: true })}
           >
             <option value=''>Choose...</option>
             <option value='M'>Male</option>
             <option value='F'>Female</option>
           </select>
+          {errors.gender && <span>This field is required</span>}
           <label htmlFor='gender'>Gender</label>
         </div>
 
@@ -50,7 +65,9 @@ const Signup = () => {
             id='email'
             name='email'
             placeholder='name@example.com'
+            {...register('email', { required: true })}
           />
+          {errors.email && <span>This field is required</span>}
           <label htmlFor='email'>Email address</label>
         </div>
 
@@ -61,7 +78,9 @@ const Signup = () => {
             id='password'
             name='password'
             placeholder='Password'
+            {...register('password', { required: true })}
           />
+          {errors.password && <span>This field is required</span>}
           <label htmlFor='password'>Password</label>
         </div>
 
