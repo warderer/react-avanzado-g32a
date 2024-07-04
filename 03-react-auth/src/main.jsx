d9bfbd7aceddad5fa0e214b22380a8fa2c88d264
César Guerra <warderer@gmail.com>
2024-07-03 20:52:38 -0600

    💄  Add Bootstrap Styles

--- 03-react-auth/src/main.jsx
+++ 03-react-auth/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
