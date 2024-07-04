84de4b050399cd9e7637083159e99381076bee9c
César Guerra <warderer@gmail.com>
2024-07-03 20:36:28 -0600

    ✨ Add NavLink to Header

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -1,24 +1,47 @@
+import { NavLink } from 'react-router-dom'
 import './header.scss'
 
 const Header = () => {
+  const linkIsActive = (isActive) => isActive ? 'header__item-link--is-active' : 'header__item-link'
+
   return (
     <nav className='header'>
-      <a href='/' className='header__logo'>LOGO</a>
+      <NavLink to='/' className='header__logo'>LOGO</NavLink>
       <ul className='header__nav-list'>
         <li className='header__list-item'>
-          <a href='/' className='header__item-link header__item-link--is-active'>Home</a>
+          <NavLink
+            to='/'
+            className={({ isActive }) => linkIsActive(isActive)}
+          >Home
+          </NavLink>
         </li>
         <li className='header__list-item'>
-          <a href='/dashboard' className='header__item-link'>Dashboard</a>
+          <NavLink
+            to='/dashboard'
+            className={({ isActive }) => linkIsActive(isActive)}
+          >Dashboard
+          </NavLink>
         </li>
         <li className='header__list-item'>
-          <a href='/secret' className='header__item-link'>Secret</a>
+          <NavLink
+            to='/secret'
+            className={({ isActive }) => linkIsActive(isActive)}
+          >Secret
+          </NavLink>
         </li>
         <li className='header__list-item'>
-          <a href='/login' className='header__item-link'>Login</a>
+          <NavLink
+            to='/login'
+            className={({ isActive }) => linkIsActive(isActive)}
+          >Login
+          </NavLink>
         </li>
         <li className='header__list-item'>
-          <a href='/register' className='header__item-link'>Signup</a>
+          <NavLink
+            to='/register'
+            className={({ isActive }) => linkIsActive(isActive)}
+          >Signup
+          </NavLink>
         </li>
       </ul>
     </nav>
