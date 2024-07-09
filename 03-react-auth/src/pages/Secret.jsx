59c07960398e1e2275e18b4b78d81771e3766643
César Guerra <warderer@gmail.com>
2024-07-08 21:04:13 -0600

    ✨ Secret: Add conditional rendering per user role

--- 03-react-auth/src/pages/Secret.jsx
+++ 03-react-auth/src/pages/Secret.jsx
@@ -1,6 +1,19 @@
+import { useAuthContext } from '@/hooks/useAuth'
+
 const Secret = () => {
+  const { userPayload } = useAuthContext()
+
   return (
-    <h1>Secret</h1>
+    <>
+      <h1>Secret</h1>
+      {userPayload?.role === 'ADMIN'
+        ? <h2>Hola ADMIN</h2>
+        : <h2>Hola CUSTOMER</h2>}
+
+      {userPayload?.role === 'ADMIN' && <h4>Bienvenido Admin</h4>}
+
+      {userPayload?.role === 'CUSTOMER' && <h4>Bienvenido Customer</h4>}
+    </>
   )
 }
 export default Secret
