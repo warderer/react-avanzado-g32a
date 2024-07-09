88670f516b5bdbfe770e9c7e924f0d9f18df7220
César Guerra <warderer@gmail.com>
2024-07-08 20:54:38 -0600

    ✨ Add Protected Routes to Secret and Dashboard

--- 03-react-auth/src/routes/Index.jsx
+++ 03-react-auth/src/routes/Index.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { useAuthContext } from '@/hooks/useAuth'
 import Home from '@/pages/Home'
 import Dashboard from '@/pages/Dashboard'
 import Secret from '@/pages/Secret'
@@ -6,11 +7,19 @@ import Login from '@/pages/Login'
 import Signup from '@/pages/Signup'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/secret' element={<Secret />} />
+      <Route
+        path='/dashboard'
+        element={isAuth ? <Dashboard /> : <Navigate to='/login' />}
+      />
+      <Route
+        path='/secret'
+        element={isAuth ? <Secret /> : <Navigate to='/login' />}
+      />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
     </Routes>
